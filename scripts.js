5d94d8062ff252bd53c76b760f0b59bdc0754bf7
emery <rachelmaansell@gmail.com>
2024-03-04 01:43:53 +0000

    Script Stuff

--- scripts.js
+++ scripts.js
@@ -2,6 +2,53 @@
 /* eslint-disable no-unused-vars */
 
 /**
+ * Greeting
+ */
+
+// Get current hour
+var currentHour = new Date().getHours();
+
+// Function to determine greeting based on the hour
+function greeting(hour) {
+  var greetingText;
+  if (hour >=5 && hour <= 11) {
+    greetingText = "Good morning";
+  } else if (hour >= 12 && hour <= 16) {
+    greetingText = "Good afternoon";
+  } else if ((hour >= 17 && hour <= 23) || (hour >= 0 && hour <= 4)) {
+    greetingText = "Good evening";
+  } else {
+    greetingText = "Hello";
+  }
+  return greetingText;
+}
+
+// Set the greeting text in the HTML element with id "greeting"
+document.getElementById("greeting").innerHTML = greeting(currentHour);
+
+/**
+ * Random Images
+ */
+
+var images = [
+  "https://kingrach.github.io/YAGSP-Tokyo-Night/img/01.gif",
+  "https://kingrach.github.io/YAGSP-Tokyo-Night/img/02.gif",
+  "https://kingrach.github.io/YAGSP-Tokyo-Night/img/03.gif",
+  "https://kingrach.github.io/YAGSP-Tokyo-Night/img/04.gif",
+  "https://kingrach.github.io/YAGSP-Tokyo-Night/img/05.gif",
+  "https://kingrach.github.io/YAGSP-Tokyo-Night/img/06.gif",
+  "https://kingrach.github.io/YAGSP-Tokyo-Night/img/07.gif",
+  "https://kingrach.github.io/YAGSP-Tokyo-Night/img/08.gif"
+];
+function getImg() {
+  var img = '<img src=\"';
+  var randomIndex = Math.floor(Math.random() * images.length);
+  img += images[randomIndex];
+  img += '\" alt=\"\"/>';
+  return img;
+}
+
+/**
  * Search function
  */
 
@@ -93,3 +140,117 @@ const injectBookmarks = () => {
 }
 
 injectBookmarks()
+
+/**
+ * Weather
+ */
+
+function getWeather() {
+  // Get location
+  if ("geolocation" in navigator) {
+      // Request location
+      navigator.geolocation.getCurrentPosition(
+          function (position) {
+              // Extract lat and lon from position
+              var lat = position.coords.latitude;
+              var lon = position.coords.longitude;
+          },
+          function (error) {
+              console.error("Error getting location: ", error.message);
+          }
+      )
+  } else {
+      console.error("Geolocation is not available in this browser.");
+  }
+  var apiKey = "518139f7a0c9b125beed32a90e71ad39";
+  var city = "Mountain Ash";
+  var url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+      // Get weather data
+      const temp = data.main.temp;
+      const owIcon = data.weather.icon;
+      const currentHour = new Date().getHours();
+      // 01d - Clear Day
+      if(owIcon == "01d") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-sun"></i> <span>${data.main.temp}°</span>`
+      }
+      // 01n - Clear Night
+      if(owIcon == "01n") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-moon"></i> <span>${data.main.temp}°</span>`
+      }
+      // 02d - Few Clouds Day
+      if(owIcon == "02d") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-cloud-sun"></i> <span>${data.main.temp}°</span>`
+      }
+      // 02n - Few Clouds Night
+      if(owIcon == "02n") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-cloud-moon"></i> <span>${data.main.temp}°</span>`
+      }
+      // 03d - Scattered Clouds Day
+      if(owIcon == "03d") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-cloud-sun"></i> <span>${data.main.temp}°</span>`
+      }
+      // 03n - Scattered Clouds Night
+      if(owIcon == "03n") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-cloud-moon"></i> <span>${data.main.temp}°</span>`
+      }
+      // 04d - Broken Clouds Day
+      if(owIcon == "04d") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-cloud"></i> <span>${data.main.temp}°</span>`
+      }
+      // 04n - Broken Clouds Night
+      if(owIcon == "04n") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-cloud"></i> <span>${data.main.temp}°</span>`
+      }
+      // 09d - Shower Rain Day
+      if(owIcon == "09d") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-cloud-sun-rain"></i> <span>${data.main.temp}°</span>`
+      }
+      // 09n - Shower Rain Night
+      if(owIcon == "09n") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-cloud-moon-rain"></i> <span>${data.main.temp}°</span>`
+      }
+      // 10d - Rain Day
+      if(owIcon == "10d") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-cloud-rain"></i> <span>${data.main.temp}°</span>`
+      }
+      // 10n - Rain Night
+      if(owIcon == "10n") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-cloud-rain"></i> <span>${data.main.temp}°</span>`
+      }
+      // 11d - Thunderstorm Day
+      if(owIcon == "11d") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-cloud-bolt"></i> <span>${data.main.temp}°</span>`
+      }
+      // 11n - Thunderstorm Night
+      if(owIcon == "11n") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-cloud-bolt"></i> <span>${data.main.temp}°</span>`
+      }
+      // 13d - Snow Day
+      if(owIcon == "13d") {
+          document.getElementById("weather").innerHTML = `<i class="fa-regular fa-snowflake"></i> <span>${data.main.temp}°</span>`
+      }
+      // 13n - Snow Night
+      if(owIcon == "13n") {
+          document.getElementById("weather").innerHTML = `<i class="fa-regular fa-snowflake"></i> <span>${data.main.temp}°</span>`
+      }
+      // 50d - Mist Day
+      if(owIcon == "50d") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-smog"></i> <span>${data.main.temp}°</span>`
+      }
+      //50n - Mist Night
+      if(owIcon == "50n") {
+          document.getElementById("weather").innerHTML = `<i class="fa-solid fa-smog"></i> <span>${data.main.temp}°</span>`
+      }
+  })
+  .catch(error => console.log(error));
+}
+
+// Call getWeather initially
+getWeather();
+
+// Call getWeather function every 1 second
+setInterval(getWeather, 1000);
